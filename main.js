3210ddb869aea1b11975e64bf889fd2b585f6601
Acer <aa>
2022-04-24 17:46:06 +0530

    final

--- main.js
+++ main.js
@@ -1,119 +1,119 @@
 new fullpage("#fullpage", {
-  autoScrolling: true,
-  continuousVertical: true,
-  scrollingSpeed: 600,
+    autoScrolling: true,
+    continuousVertical: true,
+    scrollingSpeed: 600,
 });
 
 let xyz = document.getElementById("main");
 xyz.addEventListener("click", () => {
-  xyz.scrollLeft =
-    xyz.scrollLeft >= document.body.scrollWidth ? 0 : document.body.scrollWidth;
+    xyz.scrollLeft =
+        xyz.scrollLeft >= document.body.scrollWidth ? 0 : document.body.scrollWidth;
 });
 
 let W = document.getElementById("pica");
 W.addEventListener("load", () => {
-  OG.call(W, 1, 6);
+    OG.call(W, 1, 6);
 });
 let X = document.getElementById("picb");
 X.addEventListener("load", () => {
-  OG.call(X, 7, 12);
+    OG.call(X, 7, 12);
 });
 let Y = document.getElementById("picc");
 Y.addEventListener("load", () => {
-  OG.call(Y, 13, 19);
+    OG.call(Y, 13, 19);
 });
 let Z = document.getElementById("picd");
 Z.addEventListener("load", () => {
-  OG.call(Z, 20, 27);
+    OG.call(Z, 20, 27);
 });
 
 function OG(a, b) {
-  let flag;
-  let images = [
-    "0.webp",
-    "1.webp",
-    "2.webp",
-    "3.webp",
-    "4.webp",
-    "5.webp",
-    "6.webp",
-    "7.webp",
-    "8.webp",
-    "9.webp",
-    "10.webp",
-    "11.webp",
-    "12.webp",
-    "13.webp",
-    "14.webp",
-    "15.webp",
-    "16.webp",
-    "17.webp",
-    "18.webp",
-    "19.webp",
-    "20.webp",
-    "21.webp",
-    "22.webp",
-    "23.webp",
-    "24.webp",
-    "25.webp",
-    "26.webp",
-    "27.webp",
-  ];
+    let flag;
+    let images = [
+        "0.webp",
+        "1.webp",
+        "2.webp",
+        "3.webp",
+        "4.webp",
+        "5.webp",
+        "6.webp",
+        "7.webp",
+        "8.webp",
+        "9.webp",
+        "10.webp",
+        "11.webp",
+        "12.webp",
+        "13.webp",
+        "14.webp",
+        "15.webp",
+        "16.webp",
+        "17.webp",
+        "18.webp",
+        "19.webp",
+        "20.webp",
+        "21.webp",
+        "22.webp",
+        "23.webp",
+        "24.webp",
+        "25.webp",
+        "26.webp",
+        "27.webp",
+    ];
 
-  this.addEventListener("mouseover", () => {
-    flag = true;
-    ab.call(this);
-  });
+    this.addEventListener("mouseover", () => {
+        flag = true;
+        ab.call(this);
+    });
 
-  this.addEventListener("mouseout", () => {
-    flag = false;
-  });
+    this.addEventListener("mouseout", () => {
+        flag = false;
+    });
 
-  function ab() {
-    const context = this;
+    function ab() {
+        const context = this;
 
-    function sleep(ms) {
-      return new Promise((resolve) => setTimeout(resolve, ms));
-    }
-
-    async function delay() {
-      let m = a;
-      while (++m <= b) {
-        if (flag) {
-          context.src = images[m];
-          await sleep(500);
-          m = m === b ? a : m;
-        } else {
-          break;
+        function sleep(ms) {
+            return new Promise((resolve) => setTimeout(resolve, ms));
         }
-      }
-    }
 
-    delay();
-  }
+        async function delay() {
+            let m = a;
+            while (++m <= b) {
+                if (flag) {
+                    context.src = images[m];
+                    await sleep(500);
+                    m = m === b ? a : m;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        delay();
+    }
 }
 
 let sections = document.querySelectorAll(".image");
 let options = {
-  threshold: 0.5,
+    threshold: 0.5,
 };
 
 let callback = (entries) => {
-  entries.forEach((entry) => {
-    let target = entry.target;
-    if (entry.intersectionRatio >= 0.2) {
-      target.classList.add("meow");
-      target.classList.add("o");
-    } else {
-      target.classList.remove("meow");
-    }
-  });
+    entries.forEach((entry) => {
+        let target = entry.target;
+        if (entry.intersectionRatio >= 0.2) {
+            target.classList.add("meow");
+            target.classList.add("o");
+        } else {
+            target.classList.remove("meow");
+        }
+    });
 };
 
 let observer = new IntersectionObserver(callback, options);
 
 sections.forEach((section) => {
-  observer.observe(section);
+    observer.observe(section);
 });
 
 let sections2 = document.getElementById("blue");
@@ -125,67 +125,67 @@ let sections5 = document.getElementById("three");
 let sections6 = document.getElementById("logo");
 
 sections2.addEventListener("click", () => {
-  sections2.classList.add("color_drop");
+    sections2.classList.add("color_drop");
 
-  setTimeout(myfunction, 2000);
+    setTimeout(myfunction, 2000);
 
-  function myfunction() {
-    sections3.classList.add("blue_strip");
-    sections4.classList.add("blue_strip");
-    sections5.classList.add("blue_strip");
-  }
+    function myfunction() {
+        sections3.classList.add("blue_strip");
+        sections4.classList.add("blue_strip");
+        sections5.classList.add("blue_strip");
+    }
 
-  setTimeout(myfunction2, 2000);
+    setTimeout(myfunction2, 2000);
 
-  function myfunction2() {
-    sections3.classList.add("faint_color");
-    sections4.classList.add("faint_color");
-    sections5.classList.add("faint_color");
-  }
+    function myfunction2() {
+        sections3.classList.add("faint_color");
+        sections4.classList.add("faint_color");
+        sections5.classList.add("faint_color");
+    }
 
-  sections6.classList.add("move_diagonal");
+    sections6.classList.add("move_diagonal");
 
-  setTimeout(myfunction3, 5000);
+    setTimeout(myfunction3, 5000);
 
-  function myfunction3() {
-    sections2.classList.remove("color_drop");
-    sections2.classList.add("next");
+    function myfunction3() {
+        sections2.classList.remove("color_drop");
+        sections2.classList.add("next");
 
-    sections3.classList.remove("blue_strip");
-    sections4.classList.remove("blue_strip");
-    sections5.classList.remove("blue_strip");
+        sections3.classList.remove("blue_strip");
+        sections4.classList.remove("blue_strip");
+        sections5.classList.remove("blue_strip");
 
-    sections3.classList.remove("faint_color");
-    sections4.classList.remove("faint_color");
-    sections5.classList.remove("faint_color");
+        sections3.classList.remove("faint_color");
+        sections4.classList.remove("faint_color");
+        sections5.classList.remove("faint_color");
 
-    sections3.classList.add("og");
-    sections4.classList.add("og");
-    sections5.classList.add("og");
+        sections3.classList.add("og");
+        sections4.classList.add("og");
+        sections5.classList.add("og");
 
-    sections6.classList.remove("move_diagonal");
-    sections6.classList.add("next");
-  }
+        sections6.classList.remove("move_diagonal");
+        sections6.classList.add("next");
+    }
 });
 
 let roundLogEl = document.querySelector(".dot");
 
 let callback2 = (entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0 && entry.intersectionRatio < 0.75) {
-      anime({
-        targets: roundLogEl,
-        innerHTML: [0, 50],
-        easing: "easeInOutQuad",
-        round: 1,
-        duration: 2800,
-      });
-    }
-  });
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0 && entry.intersectionRatio < 0.75) {
+            anime({
+                targets: roundLogEl,
+                innerHTML: [0, 50],
+                easing: "easeInOutQuad",
+                round: 1,
+                duration: 2800,
+            });
+        }
+    });
 };
 
 let observer2 = new IntersectionObserver(callback2, options);
 
 sections.forEach((roundLogEl) => {
-  observer2.observe(roundLogEl);
+    observer2.observe(roundLogEl);
 });
